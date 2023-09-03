adc3e0c31a3adf984b2da87af84bb2ffdf1f1673
Daria Kulikova <kdariakul@gmail.com>
2023-09-03 17:31:49 +0300

    the test was divided into a few tests

--- src/test/java/UserLogoutTest.java
+++ src/test/java/UserLogoutTest.java
@@ -1,42 +1,41 @@
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
-import model.RegistrationPage;
+import model.MainPage;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import static model.LoginPage.*;
-import static model.MainPage.PROFILE_LINK;
-import static model.MainPage.SET_BURGER_HEADER;
+import static model.MainPage.*;
 import static model.ProfilePage.*;
-import static model.RegistrationPage.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class UserRegistrationLoginAndLogoutTest {
+public class UserLogoutTest {
     private WebDriver driver;
     private User user;
+    private UserClient userClient;
+    private String accessToken;
 
     @Before
     public void beforeTest() {
         driver = BrowserDriverSetting.browserDriverSetUp();
         user = UserGenerator.getRandomUser();
-    }
-
-    @Test
-    @DisplayName("Регистрация пользователя, вход и выход.")
-    public void userRegistrationLoginAndLogoutTest(){
-        RegistrationPage page = new RegistrationPage(driver);
-        page.openPage(REGISTRATION_PAGE_URL);
-        page.findElementAndInputData(FIELD_NAME, user.getName());
-        page.findElementAndInputData(FIELD_EMAIL, user.getEmail());
-        page.findElementAndInputData(FIELD_PASSWORD, user.getPassword());
-        page.findAndClickElement(REGISTRATION_BUTTON);
-        assertTrue(page.isElementDisplayed(page.findElement(LOGIN_HEADER)));
+        userClient = new UserClient();
+        ValidatableResponse createResponse = userClient.createUser(user);
+        accessToken = createResponse.extract().path("accessToken");
+        MainPage page = new MainPage(driver);
+        page.openPage(MAIN_PAGE_URL);
+        page.findAndClickElement(SIGN_IN_ACCOUNT_BUTTON);
         page.findElementAndInputData(FIELD_EMAIL_LOGIN, user.getEmail());
         page.findElementAndInputData(FIELD_PASSWORD_LOGIN, user.getPassword());
         page.findAndClickElement(LOGIN_BUTTON);
-        assertTrue(page.isElementDisplayed(page.findElement(SET_BURGER_HEADER)));
+    }
+
+    @Test
+    @DisplayName("Выход пользователя из аккаунта")
+    public void UserLogoutTest(){
+        MainPage page = new MainPage(driver);
         page.findAndClickElement(PROFILE_LINK);
         assertEquals(user.getName(), page.findElement(FIELD_NAME_PROFILE).getAttribute("value"));
         assertEquals(user.getEmail().toLowerCase(), page.findElement(FIELD_EMAIL_PROFILE).getAttribute("value"));
@@ -47,8 +46,8 @@ public class UserRegistrationLoginAndLogoutTest {
     @After
     public void afterTest() {
         driver.quit();
-        UserClient userClient = new UserClient();
-        ValidatableResponse loginResponse = userClient.loginUser(UserCredentials.from(user));
-        userClient.deleteUser(loginResponse.extract().path("accessToken"));
+        userClient.deleteUser(accessToken);
     }
 }
+
+
